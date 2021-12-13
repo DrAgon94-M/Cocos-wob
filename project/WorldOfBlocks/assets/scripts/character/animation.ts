0a8ff26f09b87542a02c72a42950ffc7dcdecdd6
DrAgon94 <2170106485@qq.com>
2021-12-13 23:37:18 +0800

    删除了部分调用_tween的接口

--- project/WorldOfBlocks/assets/scripts/character/animation.ts
+++ project/WorldOfBlocks/assets/scripts/character/animation.ts
@@ -1,5 +1,8 @@
 import { animation, IQuatLike, Node, Quat, Size, Tween, tween, TweenSystem, UITransform, Vec2, Vec3 } from "cc";
 
+const SCALE_GROUP = 1;
+const ROTATION_GROUP = 2;
+
 class AnimationInfo {
     readonly name: string;
     readonly tween: Tween<Node>;
@@ -21,13 +24,8 @@ class AnimationDefineData {
 }
 
 class AnimationDefine {
-    static readonly stop = new AnimationDefineData("stop", 100);
-    static readonly toOriginScale = new AnimationDefineData("toOriginScale", 2);
-    static readonly toOriginRotation = new AnimationDefineData("toOriginRotation", 1);
-    static readonly idle = new AnimationDefineData("idle", 2);
-    static readonly move = new AnimationDefineData("move", 1);
-    static readonly oneJumpStart = new AnimationDefineData("oneJumpStart", 2);
-    static readonly oneJumpLeaveGround = new AnimationDefineData("oneJumpLeaveGround", 2);
+    static readonly oneJumpStart = new AnimationDefineData("oneJumpStart", 1);
+    static readonly oneJumpLeaveGround = new AnimationDefineData("oneJumpLeaveGround", 1);
     static readonly doubleJumpStart = new AnimationDefineData("doubleJumpStart", 2);
     static readonly doubleJumpRotate = new AnimationDefineData("doubleJumpRotate", 2);
     static readonly fall = new AnimationDefineData("fall", 1);
@@ -46,6 +44,7 @@ export class Animation {
 
     private _originSize: Size = new Size(100, 100);
     private _originScale: Vec3 = new Vec3(0.9, 1.4, 1);
+    private _standEuler: number = 0;
     private _idleScale: Vec3 = new Vec3(0.95, 1.35, 1);
     private _moveMaxAngle: number = 10;
     private _oneJumpStartScale: Vec3 = new Vec3(1.1, 1.2, 1);
@@ -61,22 +60,16 @@ export class Animation {
         return this._model.eulerAngles.y;
     }
 
-    private get _standEuler() {
-        return new Vec3(0, this._curEuler_Y, 0);
-    }
-
     async playStop() {
-        this._tween(AnimationDefine.stop)
-            ?.to(0.1, { scale: this._originScale, eulerAngles: this._standEuler }, { easing: "linear" })
-            .start();
+
     }
 
     playIdle() {
-        if (this._model.eulerAngles != this._standEuler) {
+        if (this._model.eulerAngles.z != this._standEuler) {
             this._toOriginRotation();
         }
 
-        this._tween(AnimationDefine.idle)
+        this._tween2("idle", SCALE_GROUP)
             ?.to(0.8, { scale: this._idleScale }, { easing: "cubicOut" })
             .to(0.8, { scale: this._originScale }, { easing: "cubicOut" })
             .union()
@@ -92,23 +85,11 @@ export class Animation {
 
         let angle = speedScale * this._moveMaxAngle;
 
-        this._tween(AnimationDefine.move)
-            ?.to(0.5, { eulerAngles: new Vec3(0, this._curEuler_Y, -angle) }, { easing: "linear" })
-            .union()
-            .repeatForever()
-            .start()
+        this._playRotation("move", -angle);
     }
 
     playOneJumpStart() {
-        return new Promise<void>((reslove, reject) => {
-            this._tween(AnimationDefine.oneJumpStart)
-                ?.to(0.1, { scale: this._oneJumpStartScale }, {
-                    easing: "linear", onComplete: () => {
-                        reslove();
-                    }
-                })
-                .start();
-        })
+        return this._playScale("oneJumpStart", this._oneJumpStartScale);
     }
 
     async playOneJumpLeaveGround() {
@@ -126,29 +107,6 @@ export class Animation {
 
     async playDoubleJumpStart() {
         return new Promise<void>((reslove, reject) => {
-
-            this._tween(AnimationDefine.doubleJumpStart)
-                ?.to(0.1, { scale: this._doubleJumpStartScale }, {
-                    easing: "linear", onComplete: () => {
-                        this._setAnchor(0.5, 0.5)
-                        this._tween(AnimationDefine.doubleJumpRotate)
-                            ?.to(0.25, { eulerAngles: new Vec3(0, this._curEuler_Y, -180) }, {
-                                easing: "linear", onComplete: () => {
-                                    this._model.eulerAngles = this._standEuler;
-                                }
-                            })
-                            .repeat(2)
-                            .union()
-                            .start();
-                    }
-                })
-                .start()
-
-            setTimeout(() => {
-                this._model.eulerAngles = this._standEuler;
-                this._setAnchor(0.5, 0)
-                reslove()
-            }, 0.35 * 1000);
 
         });
     }
@@ -179,14 +137,14 @@ export class Animation {
         return this._startNewAnim(new AnimationDefineData(animName, group)).tween;
     }
 
-    private _isShowing(animName : string, group : number) {
-        let animData = this._curAnims.get(group);
-        //如果该缓动已经播放完了，直接返回false
+    private _isShowing(animName: string, group: number) {
+        let animInfo = this._curAnims.get(group);
+        //如果该组的缓动已经播放完了，直接返回false
         let action = TweenSystem.instance.ActionManager.getActionByTag(group, this._model);
         if (!action || action.isDone())
             return false;
         //如果没播放完，查看是不是同一个名字，是返回true；否则返回false
-        return animData?.name == animName;
+        return animInfo?.name == animName;
     }
 
     private _stopOldAnim(group: number) {
@@ -201,27 +159,33 @@ export class Animation {
     }
 
     private _toOriginScale() {
-        this._tween(AnimationDefine.toOriginScale)
-            ?.to(0.1, { scale: this._originScale })
-            .start();
+        if (this._model.scale.equals(this._originScale))
+            return null;
+
+        return this._playScale("toOriginnScale", this._originScale);
     }
 
     private _toOriginRotation() {
-        this._tween(AnimationDefine.toOriginScale)
-            ?.to(0.1, { eulerAngles: this._standEuler })
-            .start();
+        if (this._model.eulerAngles.z == this._standEuler)
+            return null;
+
+        return this._playRotation("toOriginRotation", this._standEuler);
     }
 
-    private _playScale(scaleX: number, scaleY: number, duration: number = 0.1) {
-        tween(this._model)
-            .to(duration, { scale: new Vec3(0, scaleY, scaleX) })
-            .start();
+    private _playScale(animName: string, scale: Vec3, duration: number = 0.1) {
+        return new Promise<void>((resolve, reject) => {
+            this._tween2(animName, SCALE_GROUP)
+                ?.to(duration, { scale: scale }, { easing: "linear", onComplete: () => resolve() })
+                .start();
+        });
     }
 
-    private _playRotation(euler: number, duration: number = 0.1) {
-        tween(this._model)
-            .to(duration, { eulerAngles: new Vec3(0, this._curEuler_Y, euler) })
-            .start();
+    private _playRotation(animName: string, euler: number, duration: number = 0.1) {
+        return new Promise<void>((resolve, reject) => {
+            this._tween2(animName, ROTATION_GROUP)
+                ?.to(duration, { eulerAngles: new Vec3(0, this._curEuler_Y, euler) }, { easing: "linear", onComplete: () => resolve() })
+                .start();
+        });
     }
 
     private _setAnchor(x: number, y: number) {
